2053b4a10eb3fdea6137b57986404642a70f546b
Christian Cain <christian22cain@yahoo.com>
2019-12-23 13:16:01 -0600

    moved conditional links to helpers

--- TODO.md
+++ TODO.md
@@ -1,4 +1,1 @@
-1. Information - conditionally add logout link
-2. Can only go to '/order' if loggedin
-    2.A later needs to be able to only view your orders
-3. Add functionality of 1 to helpers
+1. Only show orders that are yours
--- app/helpers/view_helpers.rb
+++ app/helpers/view_helpers.rb
@@ -6,4 +6,50 @@ class ViewHelpers
     def self.is_logged_in?(session)
         !!session[:user_id]
     end
+
+    def self.show_editor_links(session)
+        links = ["information"]
+
+        if is_logged_in?(session)
+            links.push("orders", "logout")
+        else
+            links.push("login")
+        end
+
+        links.map do |l|
+            "<li><a href='/#{l}'>#{l.capitalize()}</a></li>"
+        end.join(" ")
+    end
+
+    def self.show_info_links(session)
+        links = ["editor"]
+
+        if is_logged_in?(session)
+            links.push("orders", "logout")
+        else
+            links.push("login")
+        end
+
+        links.map do |l|
+            link = '/' + l
+            (l == "editor") ? link = '/' : link = l
+            "<li><a href='#{link}'>#{l.capitalize()}</a></li>"
+        end.join(" ")
+    end
+
+    def self.show_orders_links(session)
+        links = ["editor"]
+
+        if is_logged_in?(session)
+            links.push("information", "logout")
+        else
+            links.push("login")
+        end
+
+        links.map do |l|
+            link = '/' + l
+            (l == "editor") ? link = '/' : link = l
+            "<li><a href='#{link}'>#{l.capitalize()}</a></li>"
+        end.join(" ")
+    end
 end
