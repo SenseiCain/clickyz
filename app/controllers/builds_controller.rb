2553a96cb0ee1e40d6e9749408b7db1ed4645f59
Christian Cain <christian22cain@yahoo.com>
2020-01-10 14:53:51 -0600

    move create build to build model

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -8,6 +8,27 @@ class BuildController < ApplicationController
         end
     end
 
+    get "/edit" do
+        @build = getBuild(params)
+
+        redirect_if_not_authorized(@build)
+
+        erb :edit_build
+    end
+
+    post "/builds" do
+        if session[:user_id]
+            #Move to Build Model - Create Build
+            Build.create_with_jpg(params, current_user)
+            session.delete(:keyboard_data)
+
+            redirect to '/builds'
+        else
+            session[:keyboard_data] = params.except("svg")
+            redirect to '/login'
+        end
+    end
+
     patch "/builds/:id" do
         build = getBuild(params)
 
@@ -41,30 +62,6 @@ class BuildController < ApplicationController
         redirect to '/builds'
     end
 
-    post "/builds" do
-        if session[:user_id]
-            #Move to Build Model - Create Build
-            build = Build.create(name: params["keyboard_name"], case: params["case"], cable: params["cable"], primary_color: params["keycaps_primary"], alt_color: params["keycaps_alt"])
-            build.user = current_user
-
-            convert_svg_to_jpg(params, build)
-            session.delete(:keyboard_data)
-
-            redirect to '/builds'
-        else
-            session[:keyboard_data] = params.except("svg")
-            redirect to '/login'
-        end
-    end
-
-    get "/edit" do
-        @build = getBuild(params)
-
-        redirect_if_not_authorized(@build)
-
-        erb :edit_build
-    end
-
     
 
     helpers do
@@ -85,34 +82,9 @@ class BuildController < ApplicationController
         end
 
         #Move these to Build Model
-        def delete_jpg(build)
-            File.delete("public/images/keyboard_saves/#{build.img_file}")
-        end
+        
     
-        def convert_svg_to_jpg(params, build)
-            #generates svg as placeholder
-            rand_num = rand(1000)
-            filepath = "lib/temp_svgs/temp_svg_#{rand_num}.svg"
-            temp_file = File.open(filepath, "w")  do |f| 
-                text_2 = params[:svg] + '</svg>'
-                text_2.slice! "<svg>"
         
-                f.write(File.open('lib/svgs/metadata.txt', 'r').read)
-                f.write(text_2)
-            end
-    
-            #converts svg to jpg & save
-            image = MiniMagick::Image.open(filepath)
-            image.format "jpg"
-            image.write "public/images/keyboard_saves/keyboard_#{rand_num}.jpg"
-        
-            #delete placeholder svg
-            File.delete(filepath)
-        
-            #assign file to build
-            build.img_file = "keyboard_#{rand_num}.jpg"
-            build.save
-        end
 
     end
 end
