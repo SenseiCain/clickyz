6bc46f5a497d1fefe3d56161e93458c3ed529fa3
Christian Cain <christian22cain@yahoo.com>
2020-05-30 20:02:13 -0500

    encodes db url

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,8 @@ configure :development do
 end
  
  configure :production do
-  db = URI.parse('postgres://wwsalxzfieypup:314499cc62165c8b82cc6f51b31aea34da2127a10e1a495735b0f451d6e4b248@ec2-54-86-170-8.compute-1.amazonaws.com:5432/dc72pdva0nbjj6')
+  encoded_url = URI.encode('postgres://wwsalxzfieypup:314499cc62165c8b82cc6f51b31aea34da2127a10e1a495735b0f451d6e4b248@ec2-54-86-170-8.compute-1.amazonaws.com:5432/dc72pdva0nbjj6')
+  db = URI.parse(encoded_url)
  
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
